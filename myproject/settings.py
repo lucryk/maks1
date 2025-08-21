2e7e6f56ed68383af2f6e23b9cc8bc0b0e42739b
Kuznetsov Matvey <klopkeshamatka@mail.ru>
2025-08-21 14:01:16 +0300

    chg

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-ju2mt2*+t+6+6mnrho5=+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['yourusername.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['maksimkuznetsov.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for myproject project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+# Добавьте путь к вашему проекту
+path = '/home/maksimkuznetsov/maks1'
+if path not in sys.path:
+    sys.path.append(path)
+
+# Укажите настройки Django
+os.environ['DJANGO_SETTINGS_MODULE'] = 'myproject.settings'
+
+# Импортируйте и настройте WSGI приложение
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
-
 application = get_wsgi_application()
